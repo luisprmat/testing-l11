780adfd7848d50003dcce5457c677db5eff82e62
Luis Parrado <luisprmat@gmail.com>
2024-03-10 09:51:16 -0500

    Remove tests help comments

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -11,19 +11,12 @@ test('homepage contains empty table', function () {
 });
 
 test('homepage contains non empty table', function () {
-    // Arrange
     Product::create([
         'name' => 'Product 1',
         'price' => 123,
     ]);
-    // --  End Arrange --
 
-    // Act
-    $response = get('/products');
-    // -- End Act --
-
-    // Assert
-    $response->assertStatus(200)
+    get('/products')
+        ->assertStatus(200)
         ->assertDontSee(__('No products found'));
-    // -- End Assert --
 });
