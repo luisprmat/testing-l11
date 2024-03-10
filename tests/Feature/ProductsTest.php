5ea5646c929184be0d5c5b213c704db246e99c13
Luis Parrado <luisprmat@gmail.com>
2024-03-10 10:15:56 -0500

    Use assertViewHas()

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
 
 use function Pest\Laravel\get;
 
@@ -11,7 +12,7 @@ test('homepage contains empty table', function () {
 });
 
 test('homepage contains non empty table', function () {
-    Product::create([
+    $product = Product::create([
         'name' => 'Product 1',
         'price' => 123,
     ]);
@@ -19,5 +20,6 @@ test('homepage contains non empty table', function () {
     get('/products')
         ->assertStatus(200)
         ->assertDontSee(__('No products found'))
-        ->assertSee('Product 1');
+        ->assertSee('Product 1')
+        ->assertViewHas('products', fn (Collection $collection) => $collection->contains($product));
 });
