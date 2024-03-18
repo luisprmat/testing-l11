784747d335a470abb603cc308061ffdb75ec1e07
Luis Parrado <luisprmat@gmail.com>
2024-03-18 18:45:13 -0500

    Testing APIs and JSON Data

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request)
+    {
+        return Product::create($request->validated());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
---
+++ tests/Feature/Api/ProductsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Product;
+
+use function Pest\Laravel\getJson;
+use function Pest\Laravel\postJson;
+
+test('api returns products list', function () {
+    $product = Product::factory()->create();
+
+    $res = getJson('api/products')
+        ->assertJson([$product->toArray()]);
+
+    expect($res->content())
+        ->json()
+        ->toHaveCount(1);
+});
+
+test('api product store successful', function () {
+    $product = [
+        'name' => 'Product 1',
+        'price' => 123,
+    ];
+
+    postJson('api/products', $product)
+        ->assertStatus(201)
+        ->assertJson($product);
+});
+
+test('api product invalid store returns error', function () {
+    $product = [
+        'name' => '',
+        'price' => 123,
+    ];
+
+    postJson('api/products', $product)
+        ->assertStatus(422);
+});
