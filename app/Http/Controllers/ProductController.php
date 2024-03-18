936eae894fab97a2044fb64370869bd6d062c1ff
Luis Parrado <luisprmat@gmail.com>
2024-03-17 23:48:12 -0500

    Update Product: Test Validation Errors

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
 use App\Models\Product;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
@@ -31,5 +32,12 @@ class ProductController extends Controller
     public function edit(Product $product): View
     {
         return view('products.edit', compact('product'));
+    }
+
+    public function update(UpdateProductRequest $request, Product $product): RedirectResponse
+    {
+        $product->update($request->validated());
+
+        return redirect()->route('products.index');
     }
 }
